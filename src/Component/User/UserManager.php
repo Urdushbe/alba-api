df2a4f2c59398c24408789affc6f9c0618d82554
Urdushbek <Urdushbek.officla@gmail.com>
2023-04-11 02:41:19 +0900

    Create User Manager

---
+++ src/Component/User/UserManager.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Component\User;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+
+class UserManager
+{
+    public function __construct(private EntityManagerInterface $entityManager)
+    {
+
+    }
+
+    public function save(User $user, bool $isNeedFlush = false)
+    {
+        $this->entityManager->persist($user);
+        if ($isNeedFlush) {
+            $this->entityManager->flush();
+        }
+    }
+}
--- src/Controller/UserCreateAction.php
+++ src/Controller/UserCreateAction.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Component\User\UserFactory;
+use App\Component\User\UserManager;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserCreateAction extends AbstractController
 {
-    public function __construct(private UserFactory $userFactory)
+    public function __construct(private UserFactory $userFactory, private UserManager $userManager)
     {
     }
 
-    public function __invoke(User $data): void
+    public function __invoke(User $data): User
     {
         $user = $this->userFactory->create(
             $data->getEmail(),
@@ -22,9 +23,10 @@ class UserCreateAction extends AbstractController
             $data->getFullName(),
             $data->getTel()
         );
+        $this->userManager->save($user, true);
 
-        print_r($user);
-        exit;
+        return $user;
+
     }
 
 }
